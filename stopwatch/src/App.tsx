9c13c8ec57a8cb6c997c10a1940b5e41c8d90f7b
Naina <160093996+naina3009@users.noreply.github.com>
2024-08-06 17:24:42 +0530

    Initial Set up Stop Watch App

---
+++ stopwatch/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import StopWatch from './screen/stop-watch/StopWatch';
+
+function App() {
+  return (
+  <StopWatch/>
+  );
+}
+
+export default App;
---
+++ stopwatch/src/screen/stop-watch/StopWatch.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+
+const StopWatch : FC = ()=>{
+    return(
+        <>
+        <h2>Stop Watch</h2>
+        
+        </>
+    )
+} 
+export default StopWatch;
