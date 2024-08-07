c946b8df66672d0a797e758dab0e76736f7ed6a8
Naina <160093996+naina3009@users.noreply.github.com>
2024-08-07 09:25:50 +0530

    Added logic for Timer

--- stopwatch/src/screen/stop-watch/StopWatch.tsx
+++ stopwatch/src/screen/stop-watch/StopWatch.tsx
@@ -4,22 +4,25 @@ import Buttons from "../../components/button/Buttons";
 
 const StopWatch : FC = ()=>{
     const [count,setCount] = useState<number>(0)
+    const [isStart,setIsStart] = useState<boolean>(false)
  
     const handleStart = ()=>{
-       
+        setIsStart(true)
     }
     useEffect(()=>{
-       let intervel=  setTimeout(()=>{
-            setCount(count+1)
-          },1000)
-    },[count])
+        if(isStart){
+            setTimeout(()=>{
+                setCount(count+1)
+              },100)
+        }
+    },[isStart,count])
     const handleStop = ()=>{
-        // clearInterval(intervel)
+        setIsStart(false)
      }
     return(
         <>
         <h2 className="d-flex justify-content-center mt-4">
-            Stop Watch - {count}
+            Stop Watch / Timer - {count}
         </h2>
         <div className="d-flex justify-content-center">
             <Buttons onClick={handleStart} label={"Start"} className=" btn btn-primary m-2  w-25"/>
